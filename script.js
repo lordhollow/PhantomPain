5c15558ed19488fedda12d7dd9465b7bed985b2d
LordHllow <HollowCircus@gmail.com>
2012-05-22 16:15:44 +0900

    pref.jsをscript.jsと合体

--- script.js
+++ script.js
@@ -58,6 +58,113 @@ Array.prototype.include = function prototype_include(val) {
 }
 var $=function prototype_getElementById(id){return document.getElementById(id);}
 
+/* ■スキンの設定■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ */
+var SkinPref = {
+
+	_skinName: "PhantomPain",
+	_storage: localStorage,
+	
+	_resolvePrefName: function(aPrefName){
+		return this._skinName + "_" + aPrefName;
+	},
+	
+	getStr: function(aPrefName, aDefaultValue){
+		var item = this._storage.getItem(this._resolvePrefName(aPrefName));
+		if(item == null) return aDefaultValue || "";
+		return item;
+	},
+	setStr: function(aPrefName, aValue){
+		var value = String(aValue);
+		this._storage.setItem(this._resolvePrefName(aPrefName), value);
+		return value;
+	},
+
+	getInt: function(aPrefName, aDefaultValue){
+		var item = this._storage.getItem(this._resolvePrefName(aPrefName));
+		if(item == null) return aDefaultValue || 0;
+		return parseInt(item);
+	},
+	setInt: function(aPrefName, aValue){
+		var value = parseInt(aValue);
+		this._storage.setItem(this._resolvePrefName(aPrefName), value);
+		return value;
+	},
+		
+	getBool: function(aPrefName, aDefaultValue){
+		var item = this._storage.getItem(this._resolvePrefName(aPrefName));
+		if(item == null) return aDefaultValue || false;
+		return (item == "true");
+	},
+	setBool: function(aPrefName, aValue){
+		var value = (aValue) ? "true" : "false";
+		this._storage.setItem(this._resolvePrefName(aPrefName), value);
+		return value;
+	}
+
+};
+
+/* ■共通の設定■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ */
+var CommonPref = {
+
+	_identifier: new String("UNKNOWN"),
+	
+	_storage: localStorage,
+	
+	_resolvePrefName: function CommonPref__resolvePrefName(aPrefName){
+		return "bbs2chSkin.common." + aPrefName + this._identifier;
+	},
+	
+	setIdentifier: function CommonPref_setIdentifier(aThreadURL) {
+		if (aThreadURL.match(/machi\.to/)) {
+			//まちBBS
+			var _bbskey ="";
+			var _thread = "";
+			if (document.location.href.match(/BBS=([^&]+)/i)) {
+				_bbskey = RegExp.$1;
+			}
+			if (document.location.href.match(/KEY=([0-9]+)/i)) {
+				_thread = RegExp.$1;
+			}
+			this._identifier = "machi." + _bbskey + "." + _thread;
+		} else {
+			//2ch
+			if (aThreadURL.match(/([^\/]+)\/([^\/]+)\/$/)) {
+				this._identifier = RegExp.$1 + "." + RegExp.$2;
+			}
+		}
+	},
+	
+	getThreadObjectKey: function(objName)
+	{
+		return "bbs2chSkin.common." + objName + "." + this._identifier;
+	},
+	getGlobalObjectKey: function(objName)
+	{
+		return "bbs2chSkin.common." + objName;
+	},
+	
+	//objName = ブックマーク：bm, ピックアップ：pk, Ignores: ig
+	writeThreadObject: function CommonPref_wroteThreadObject(objName, str)
+	{
+		var pn = "bbs2chSkin.common." + objName + "." + this._identifier;
+		this._storage.setItem(pn, str);
+	},
+	readThreadObject: function CommonPref_readThreadObject(objName,eval)
+	{
+		var pn = "bbs2chSkin.common." + objName + "." + this._identifier;
+		return this._storage.getItem(pn);
+	},
+	writeGlobalObject: function CommonPref_writeGlobalObject(objName, str)
+	{
+		var pn = "bbs2chSkin.common." + objName;
+		this._storage.setItem(pn, str);
+	},
+	readGlobalObject: function CommonPref_readGlobalObject(objName)
+	{
+		var pn = "bbs2chSkin.common." + objName;
+		return this._storage.getItem(pn);
+	},
+};
 
 /* ■イベントハンドラ■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■ */
 var EventHandlers = {
